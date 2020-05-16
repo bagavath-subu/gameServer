25da9f2371bb7d199d00e7bf59f3db3a4cfe4dc9
subramanian <subramanian>
2020-05-16 17:30:42 +0530

    port added

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 var cors = require('cors')
 const server = http.createServer(app);
 const io = socketIo(server);
-
 
 app.use(cors())
 
@@ -20,23 +19,32 @@ io.on("connection", socket => {
     socket.on('user-name', name => {
         user[socket.id] = name
         console.log(name, "  connected")
-        socket.broadcast.emit('User-connected', name)
     })
+
+    socket.on('room', (room) => {
+        socket.join(room);
+        console.log(`${user[socket.id]}  joined the room`)
+        socket.in(room).broadcast.emit('User-connected', user[socket.id])
+    });
+
+    socket.on('startGame', (room) => {
+        io.to(room).emit('startingGame')
+    });
 
     socket.on('stateChanged', (state) => {
         state.name = user[socket.id]
+        console.log("state", state)
         console.log("Users", user, "socket", socket.id, "State Changed : ", user[socket.id])
-        socket.broadcast.emit("updateState", state);
+        socket.in(state.roomId).broadcast.emit("updateState", state);
     });
 
     socket.on('bingo', () => {
         const name = user[socket.id]
-        socket.broadcast.emit("GameOver", name);
+        socket.in(room).broadcast.emit("GameOver", name);
     });
 
     socket.on("disconnect", () => console.log("Client disconnected", socket.id));
 });
-
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
